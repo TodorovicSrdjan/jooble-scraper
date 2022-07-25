63ce09eddb95d4aeb20e67fc3f5e9cd3a4ae6c11
TodorovicSrdjan <tsrdjan@pm.me>
2022-07-25 08:13:19 +0200

    add script

---
+++ config.py
@@ -0,0 +1,46 @@
+################
+# General 
+
+PROGRAM_NAME = "Jooble Scraper"
+RESOURCE_PATH = "jooble.org/api/serp/jobs"
+REQ_DELAY = 4
+
+################
+# Constats
+
+# Experience
+EXP_ANY       = 0
+EXP_INTERSHIP = 2
+
+# Date
+DATE_ANY        = 7
+DATE_1_DAY      = 8
+DATE_3_DAYS     = 2
+DATE_7_DAYS     = 3
+
+# Location
+LOC_REMOTE   = 2
+LOC_EXECT    = 4
+LOC_10_MILES = 5
+LOC_25_MILES = 6
+
+# Job type
+JTYPE_DEFAULT   = 0
+JTYPE_FULL_TIME = 1
+JTYPE_TEMPORARY = 2
+JTYPE_PART_TIME = 3
+
+################
+# Default params
+
+SEARCH        = ''
+DATE          = DATE_1_DAY
+LOCATION      = LOC_25_MILES
+COUNTRY_CODE  = "rs"
+REGION        = ''
+JOB_TYPES     = ['full', 'temp', 'part']
+EXPERIENCE    = EXP_INTERSHIP
+
+SALARY_MIN       = 1
+SALARY_MAX       = 999999
+ONLY_WITH_SALARY = False
---
+++ jooble.py
@@ -0,0 +1,258 @@
+import config
+
+import urllib3, json, argparse, csv, sys
+from datetime import datetime
+from time import sleep
+
+#################################################################
+
+class CountryCodeAction(argparse.Action):
+    def __init__(self, check_func, *args, **kwargs):
+        """
+        argparse custom action.
+        :param check_func: callable to do the real check.
+        """
+        self._check_func = check_func
+        super(CountryCodeAction, self).__init__(*args, **kwargs)
+        
+    def __call__(self, parser, namespace, values, option_string=None):
+        #print('%r %r %r' % (namespace, values, option_string))
+        
+        if isinstance(values, list):
+            values = [self._check_func(parser, v) for v in values]
+        else:
+            values = self._check_func(parser, values)
+        setattr(namespace, self.dest, values)
+    
+    @staticmethod
+    def __chk__(parser, country_code):
+        if len(country_code) > 2 or not country_code.isalpha():
+            raise ValueError("country code must have exactly 2 letters")
+        
+        return country_code
+        
+#################################################################
+
+def main(args):
+    form_data = get_form_data(args)
+    
+    if len(sys.argv) == 0:
+        form_data['country_code'] = config.COUNTRY_CODE
+    url = f"https://{form_data['country_code']}.{config.RESOURCE_PATH}"
+    del form_data['country_code']
+    
+    data = request_data(url, form_data)
+    
+    if args['export']:
+        save_to_csv(data)
+        
+'''
+Prints out program banner
+'''
+def print_program_banner():
+    l = len(config.PROGRAM_NAME)
+    line = "="*(l+4)
+    print(f"{line}\n= {config.PROGRAM_NAME} =\n{line}")
+    
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Fetch and get notification for filtered jobs from jooble.org")
+    
+    parser.add_argument('-c', '--country-code', 
+                        help='upper limit for number of days since job is posted',
+                        type=str,
+                        default='us',
+                        required='-r' in sys.argv or '--region' in sys.argv,
+                        action=CountryCodeAction,
+                        check_func=CountryCodeAction.__chk__)
+    
+    parser.add_argument('-d', '--days-ago', 
+                        help='upper limit for number of days since job is posted',
+                        type=int,
+                        default=0,
+                        dest='date',
+                        choices=[0, 1, 3, 7])
+    
+    parser.add_argument('-e', '--experience', 
+                        help='required experience for a job',
+                        type=str,
+                        default='any',
+                        choices=['any', 'intern'])
+    
+    parser.add_argument('-l', '--location', 
+                        help='distance from selected location (in miles)',
+                        type=str,
+                        default='25',
+                        choices=['remote', 'exect', '10', '25'])
+    
+    parser.add_argument('-m', '--min-salary', 
+                        help='show jobs with salary greater then MIN',
+                        type=int,
+                        default=1,
+                        dest='salaryMin', 
+                        metavar='MIN')
+    
+    parser.add_argument('-M', '--max-salary', 
+                        help='show jobs with salary lower then MAX',
+                        type=int,
+                        default=999999,
+                        dest='salaryMax', 
+                        metavar='MAX')
+    
+    parser.add_argument('-r', '--region', 
+                        help='filter by region in the country (-c or --country-code option is necessary for this option since region is part of the country)',
+                        type=str,
+                        default='',
+                        required=False)
+    
+    parser.add_argument('-s', '--search', 
+                        help='string that will be used as search bar content',
+                        type=str,
+                        default='')
+    
+    parser.add_argument('-S', '--only-with-salary', 
+                        help='exclude posts which do not have specified salary',
+                        dest='withSalary', 
+                        action='store_true')
+    
+    #parser.add_argument('-t', '--job-type', 
+                        #help='types of the job (period of employment for a job) which should be included in result',
+                        #type=list,
+                        #dest='jobTypes',
+                        #choices=['full', 'temp', 'part'], 
+                        #action='append')
+    
+    parser.add_argument('-x', '--export', 
+                        help='export fetched data into CSV file', 
+                        action='store_true')
+    
+    return vars(parser.parse_args())
+    
+def adapt_args_for_api(args):
+    if 'salaryMin' in args and 'salaryMax' in args:
+        if int(args['salaryMin']) > int(args['salaryMax']):
+            raise ValueError('Minimal salary cannot be greater then maximal salary.')
+    
+    if 'experience' in args:
+        if args['experience'] == 'any':
+            args['experience'] = config.EXP_ANY
+        elif args['experience'] == 'intern':
+            args['experience'] = config.EXP_INTERSHIP
+            
+    if 'date' in args:
+        if args['date'] == 0:
+            args['date'] = config.DATE_ANY
+        elif args['date'] == 1:
+            args['date'] = config.DATE_1_DAY
+        elif args['date'] == 3:
+            args['date'] = config.DATE_3_DAYS
+        elif args['date'] == 7:
+            args['date'] = DATE_7_DAYS 
+    
+    if 'location' in args:
+        if args['location'] == 'any':
+            del args['location']
+        elif args['location'] == 'remote':
+            args['location'] = config.LOC_REMOTE
+        elif args['location'] == 'exect':
+            args['location'] = config.LOC_EXECT
+        elif args['location'] == '10':
+            args['location'] = config.LOC_10_MILES
+        elif args['location'] == '25':
+            args['location'] = config.LOC_25_MILES 
+           
+    #if 'jobTypes' in args:
+        #for i, job_type in enumerate(args['jobTypes']):
+            #if args['jobTypes'][i] == 'full':
+                #args['jobTypes'][i] = config.JTYPE_FULL_TIME
+            #elif args['jobTypes'][i] == 'temp':
+                #args['jobTypes'][i] = config.JTYPE_TEMPORARY
+            #elif args['jobTypes'][i] == 'part':
+                #args['jobTypes'][i] = config.JTYPE_PART_TIME
+    
+    return args
+
+def get_form_data(args):
+    if len(args) < 1:
+        args = {
+                'date'       : config.DATE,
+                'experience' : config.EXPERIENCE,
+                'location'   : config.LOCATION,
+                #'jobTypes'   : config.JOB_TYPES,
+                'region'     : config.REGION,
+                'salaryMin'  : config.SALARY_MIN,
+                'salaryMax'  : config.SALARY_MAX,
+                'search'     : config.SEARCH,
+                'withSalary' : config.ONLY_WITH_SALARY
+            }
+    return adapt_args_for_api(args)
+    
+def request_data(url, form_data, npage=0):
+    jobs = []
+    to_be_fetched = 1
+    http = urllib3.PoolManager()
+        
+    while npage != to_be_fetched:
+        form_data['page'] = to_be_fetched
+        encoded_data = json.dumps(form_data).encode('utf-8')
+        print("Sending request...")
+        response = http.request(
+            'POST',
+            url,
+            body=encoded_data,
+            headers={'Content-Type':'application/json'}
+        )
+        
+        print("Server response:", response.status , encoded_data, len(jobs))
+        if response.status != 200:
+            break
+        
+        data = json.loads(response.data)['jobs']
+        print(data)
+        if len(data) == 0:
+            break
+        
+        jobs += data
+        to_be_fetched += 1
+        
+        sleep(config.REQ_DELAY)
+            
+    return jobs
+
+def save_to_csv(jobs):
+    print("Exporting data...")
+    
+    # get current time without milliseconds
+    current_time = str(datetime.now()).split('.')[0]
+    
+    # replace spaces to avoid problems with filename
+    file_name = current_time.replace(' ', '_') + '.csv'
+    
+    headers = list(jobs[0].keys()) 
+    for obj_name in ['company', 'location', 'awayData']:
+        headers += [f"{obj_name}_{key}" for key in jobs[0][obj_name].keys()]
+    
+    with open(file_name, 'w', newline = '') as csv_file:
+        csv_writer = csv.writer( csv_file, quoting=csv.QUOTE_NONNUMERIC )
+        
+        csv_writer.writerow( headers )
+        
+        for job in jobs:
+            to_add = []
+            
+            for obj_name in ['company', 'location', 'awayData']:
+                to_add += [obj_value for obj_value in job[obj_name]]
+                del job[obj_name]
+                
+            job['tags'] = ', '.join(job['tags'])
+            
+            values = list(job.values()) + to_add
+             
+             
+            csv_writer.writerow( values )
+        
+    print(f"Data is successfuly exported to '{file_name}'")    
+    
+if __name__ == '__main__':
+    args = parse_arguments()
+    print_program_banner()
+    main(args)
